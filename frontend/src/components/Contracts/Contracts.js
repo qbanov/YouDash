99805e39a1fcc5549a48508f2eabd293364cf035
Jakub Nowak <jakub.nowak@smokedsystem.com>
2025-10-31 23:07:25 +0100

    PyCharm

---
+++ frontend/src/components/Contracts/Contracts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contracts() {
+  return (
+    <div className="container-fluid py-4">
+      <h1>Umowy</h1>
+      <p>Zarządzanie umowami - w budowie</p>
+    </div>
+  );
+}
+
+export default Contracts;
